4bc3c734fe3bc7697d897019a949610b7236bdbe
ibrahim <ibrahimmohammed3284@gmail.com>
2025-08-11 01:03:13 +0530

    Added a new project AcademyApp

---
+++ AcademyApp/src/test/java/com/ibrahim/AcademyApp/AcademyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahim.AcademyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
