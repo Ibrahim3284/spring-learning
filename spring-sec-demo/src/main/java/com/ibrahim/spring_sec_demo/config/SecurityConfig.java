6816b3c1a54459c77e055e3376910801efa89ba9
ibrahim <ibrahimmohammed3284@gmail.com>
2025-08-11 18:40:19 +0530

    Enhanced security to read from DB

--- spring-sec-demo/src/main/java/com/ibrahim/spring_sec_demo/config/SecurityConfig.java
+++ spring-sec-demo/src/main/java/com/ibrahim/spring_sec_demo/config/SecurityConfig.java
@@ -1,17 +1,37 @@
 package com.ibrahim.spring_sec_demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationProvider authProvider() {
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return provider;
+    }
 
 
     // Lambda way
@@ -24,8 +44,30 @@ public class SecurityConfig {
         http.httpBasic(Customizer.withDefaults());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
+        // User password
+
         return http.build();
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//
+//        UserDetails user = User
+//                .withDefaultPasswordEncoder()
+//                .username("Ibrahim")
+//                .password("1234")
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User
+//                .withDefaultPasswordEncoder()
+//                .username("Mohammed")
+//                .password("1234")
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
 
 //    @Bean
 //    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
---
+++ spring-sec-demo/src/main/java/com/ibrahim/spring_sec_demo/model/User.java
@@ -0,0 +1,22 @@
+package com.ibrahim.spring_sec_demo.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    private int id;
+    private String username;
+    private String password;
+}
