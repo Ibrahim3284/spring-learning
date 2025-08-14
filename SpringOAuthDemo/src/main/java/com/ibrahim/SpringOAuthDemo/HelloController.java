f96342d52e10bf4d862f8329619b4f87e879dfe8
Ibrahim <ibrahimmohammed3284@gmail.com>
2025-08-14 22:15:55 +0530

    Added code for JWT OAuth, Project for OAuth2

---
+++ SpringOAuthDemo/src/main/java/com/ibrahim/SpringOAuthDemo/HelloController.java
@@ -0,0 +1,14 @@
+package com.ibrahim.SpringOAuthDemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greet() {
+        return "Welcome!!";
+    }
+}
